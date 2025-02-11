ffbc9bd37971d762e6574939bbd78c5b945c9540
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2025-02-11 17:56:19 -0500

    adding cart, search

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,4 +1,16 @@
+import { IoBed } from "react-icons/io5";
+
+import { Button } from "../ui/button";
+import Link from "next/link";
+import { Icon } from "@radix-ui/react-select";
+
 const Logo = () => {
-  return <div>Logo</div>;
+  return (
+    <Button size="icon" asChild>
+      <Link href={"/"}>
+        <IoBed className="w-6 h-6" />
+      </Link>
+    </Button>
+  );
 };
 export default Logo;
