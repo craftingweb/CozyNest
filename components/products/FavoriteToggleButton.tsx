cc9752d6edd2b37a8749670978fecbb675fd6a8b
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2025-02-13 16:18:06 -0500

    featured products

--- components/products/FavoriteToggleButton.tsx
+++ components/products/FavoriteToggleButton.tsx
@@ -1,4 +1,10 @@
-const FavoriteToggleButton = () => {
-  return <div>FavoriteToggleButton</div>;
-};
+import { FaHeart } from "react-icons/fa";
+import { Button } from "@/components/ui/button";
+function FavoriteToggleButton({ productId }: { productId: string }) {
+  return (
+    <Button size="icon" variant="outline" className="p-2 cursor-pointer">
+      <FaHeart />
+    </Button>
+  );
+}
 export default FavoriteToggleButton;
