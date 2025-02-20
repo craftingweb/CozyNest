a049fc47adefd1bcadac8e85bbf6935aaa9453b5
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2025-02-20 10:04:16 -0500

    hero section

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,4 +1,28 @@
-const Hero = () => {
-  return <div>Hero</div>;
-};
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import HeroCarousel from "./HeroCarousel";
+
+function Hero() {
+  return (
+    <section className="grid grid-cols-1 lg:grid-cols-2 gap-24 items-center">
+      <div>
+        <h1 className="max-w-2xl font-bold text-4xl tracking-tight sm:text-6xl">
+          We are changing the way people shop
+        </h1>
+        <p className="mt-8 max-w-xl text-lg leading-8 text-muted-foreground">
+          We are transforming the shopping experience by making it faster,
+          smarter, and more personalized than ever before. Our innovative
+          approach is redefining how people discover, compare, and purchase
+          products effortlessly. Say goodbye to outdated shopping habits—our
+          platform brings convenience, technology, and a seamless experience to
+          your fingertips.
+        </p>
+        <Button asChild size="lg" className="mt-10">
+          <Link href="/products">Our Products</Link>
+        </Button>
+      </div>
+      <HeroCarousel />
+    </section>
+  );
+}
 export default Hero;
