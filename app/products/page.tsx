8065346fe953136b5ed94db2769393b2aca47ace
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2025-02-24 20:20:38 -0500

    product container

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,4 +1,16 @@
-const ProductPage = () => {
-  return <div>ProductPage</div>;
-};
-export default ProductPage;
+import ProductsContainer from "@/components/products/ProductsContainer";
+
+async function ProductsPage({
+  searchParams,
+}: {
+  searchParams: { layout?: string; search?: string };
+}) {
+  const layout = searchParams.layout || "grid";
+  const search = searchParams.search || "";
+  return (
+    <>
+      <ProductsContainer layout={layout} search={search} />
+    </>
+  );
+}
+export default ProductsPage;
